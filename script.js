b13291e77646b72fdeca7da2ffa94b020da54fd0
cpu <tadzhibaevaelnura@gmail.com>
2025-12-24 06:04:22 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,74 @@
+for (i = 1; i <= 5; i++) {
+    // На каждом шаге цикла увеличиваем количество шагов на 100
+    let шаги = i * 100;
+    // Показываем, сколько шагов сделано на этом этапе
+    console.log("Я сегодня сделала " + шаги + " шагов!");
+  }
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let E = prompt("Канча санга чейин чыгарасың ?"); // Бул жерден 10 сан десем 10 санга чейин чыгарып берет
+// while(E > 1){
+//     E--;    // Бул жерден минус койуп койдум Тескери кетиш үчүн сан
+//     // alert(E + "E");
+//     console.log(E + "E");
+
+// // i = 10;
+
+
+// while (i >= 1) {
+//   console.log(i);
+//   i--;
+// }
+
+// //создание Объекта
+// let obj1 = {
+//     name : "Ramazan",
+//     age : 12,
+//     "Ramazan is puple" : true
+// }
+
+// console.log(obj1);
+// console.log(obj1["Ramazan is puple"]);
+
+
+
+// // Объект 2
+// let obj2 = {
+//     name : "Ilimbek",
+//     age : 13,
+//     "Ilimbek is programmer" : true
+// }
+
+// console.log(obj2);
+// console.log(obj2["Ilimbek is programmer"]);
+
+// // Object менен if else иштетебиз
+
+// let key = prompt("Объект жонундо эмнени билгинер келет?", "name");
+
+
+// if(obj2[key] === undefined){
+//     alert("Такого объекта нету");
+// }
+// else{obj2[key]
+//     alert(obj2.name)
+// };
+
